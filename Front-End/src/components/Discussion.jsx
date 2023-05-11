cb0d2070fc4237608d02c287f17375d5089db7d6
marsho23 <Mshouket@qa.com>
2023-05-11 16:21:35 +0100

    added star rating system

--- Front-End/src/components/Discussion.jsx
+++ Front-End/src/components/Discussion.jsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import StarIcon from '@material-ui/icons/Star';
+
 const Discussion = () => {
   const [discussions, setDiscussions] = useState([]);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [filmId, setFilmId] = useState('');
+  const [rating, setRating] = useState(0);
 
   useEffect(() => {
     fetchDiscussions();
   }, []);
 
-  // Fetch all discussions from the backend
   const fetchDiscussions = () => {
     axios
-      .get('http://localhost:5000/discussions')
+      .get('http://localhost:8080/discussion_board')
       .then((response) => {
         setDiscussions(response.data);
       })
@@ -23,20 +27,19 @@ const Discussion = () => {
       });
   };
 
-  // Create a new discussion by making a POST request to the backend
   const handleCreateDiscussion = (e) => {
     e.preventDefault();
 
     axios
-      .post('http://localhost:5000/discussions', {
+      .post('http://localhost:8080/discussion_board', {
         title,
         body,
         film_id: filmId,
-        film_rating: 0,
+        film_rating: rating,
       })
       .then((response) => {
         console.log(response.data);
-        fetchDiscussions();
+        fetchDiscussions(); 
       })
       .catch((error) => {
         console.log(error);
@@ -45,6 +48,29 @@ const Discussion = () => {
     setTitle('');
     setBody('');
     setFilmId('');
+    setRating(0);
+  };
+
+  // Handle the selection of a rating
+  const handleRatingClick = (selectedRating) => {
+    setRating(selectedRating);
+  };
+
+  // Render the star icons based on the selected rating
+  const renderStars = () => {
+    const stars = [];
+
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <StarIcon
+          key={i}
+          onClick={() => handleRatingClick(i)}
+          style={{ cursor: 'pointer', color: i <= rating ? 'yellow' : 'white' }}
+        />
+      );
+    }
+
+    return stars;
   };
 
   return (
@@ -69,6 +95,10 @@ const Discussion = () => {
           value={filmId}
           onChange={(e) => setFilmId(e.target.value)}
         />
+        <div>
+          <label>Rating: </label>
+          {renderStars()} {/* Render the star icons */}
+        </div>
         <button type="submit">Create Discussion</button>
       </form>
       <h2>Discussions</h2>
@@ -78,7 +108,7 @@ const Discussion = () => {
             <h3>{discussion.title}</h3>
             <p>{discussion.body}</p>
             <p>Film ID: {discussion.film_id}</p>
-            {/* <p>Rating: {discussion.film_rating}</p> */}
+            <p>Rating: {discussion.film_rating}</p>
           </li>
         ))}
       </ul>
@@ -87,4 +117,3 @@ const Discussion = () => {
 };
 
 export default Discussion;
-
