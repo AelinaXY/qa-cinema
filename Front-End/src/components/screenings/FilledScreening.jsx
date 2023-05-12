81afdab8d0a74703cad04cb865985ae9680592db
Stormstar109-new <Lauren@2infinity.net>
2023-05-12 14:04:12 +0100

    Add Film Showings

--- Front-End/src/components/screenings/FilledScreening.jsx
+++ Front-End/src/components/screenings/FilledScreening.jsx
@@ -4,86 +4,173 @@ import Col from "react-bootstrap/Col";
 import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import shuffle from "../functions/shuffle";
 
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
+import arrayChunk from "../functions/arrayChunk";
 
 
-const FilledScreening = ({films, selectedFilm}) => {
+const FilledScreening = ({ films, selectedFilm }) => {
 
-    // const [selectedFilmId, setSelectedFilmId] = useState(0);
-    console.log("IN FILLED SCREENING PAGE");
-    console.log(films);
-    console.log(selectedFilm);
+  // const [selectedFilmId, setSelectedFilmId] = useState(0);
+  console.log("IN FILLED SCREENING PAGE");
+  console.log(films);
+  console.log(selectedFilm);
 
 
-    //Find Film ID
-    let selectedFilmId = null;
+  //Get All Showings
+  const [showingData, setShowingData] = useState("");
+  const [error, setError] = useState("");
+  const loaded = useRef(false);
 
-    for(let i = 0; i<films.length; i++){
+  const request = ((url, setFunction, filmTitle, filmId) => {
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+    console.log(`making request for at ${url}`);
+    axios
+      .get(url, config)
+      .then((response) => {
+        setFunction(response.data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
 
-        if (films[i].film_title.replace(/\s+/g, '') === selectedFilm){
-            selectedFilmId = films[i].id;
-        }
+  })
+
+  useEffect(() => {
+    if (loaded.current === false) {
+      request(`http://localhost:8080/showings/allFilms/0`, setShowingData);
+
+      loaded.current = true
     }
 
+  }, []);
 
-    //Get All Showings
-    const [showingData, setShowingData] = useState("");
-    const [error, setError] = useState("");
-    const loaded = useRef(false);
 
-    const request = ((url, setFunction, filmTitle, filmId) =>
+  //Film Cards
+  const chunkedFilms = arrayChunk(films, 3);
+  const printArray = [];
+
+  chunkedFilms.map((filmArray) => {
+    let containedFlag = false;
+    let savedFilm;
+
+    filmArray.forEach((film) => { 
+      if(film.film_title.replace(/\s+/g, '') === selectedFilm)
     {
-      const config = {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      };
-      console.log(`making request for at ${url}`);
-      axios
-        .get(url, config)
-        .then((response) => {
-          setFunction(response.data);
-        })
-        .catch((error) => {
-          setError(error);
-        });
-
-    })
-
-    useEffect(() =>
+      savedFilm = film;
+      containedFlag = true;
+    }})
     {
-      if (loaded.current === false){
-          request(`http://localhost:8080/showings/allFilms/0`,setShowingData);
-        
-        loaded.current = true
-      }
-
-    },[]);
-
-
-    
-
-
-    if (loaded) {
-      console.log("68890");
-      if (error !== "") {
-        console.log(error);
-      } else if (showingData !== "") {
-        console.log(showingData);
-        return (
-          <>
-          <h2>{selectedFilmId}</h2>
-          </>
-        );
-
-      }
-    } else{
-      console.log("68891");
+      
     }
+
+    if(containedFlag)
+    {
+      shownCardsAdder(printArray, filmArray);
+
+      printArray.push(
+        <Row>
+          <Col className=" d-flex align-items-center justify-content-center">
+
+            <Card style={{ width: "80%"}}>
+              <Card.Body>
+                <Container>
+                  <Row>
+                    <Col>
+                      <Card.Img id="shownFilmImage" variant="top" src={`${savedFilm.film_poster}` }/>
+
+                    </Col>
+
+                    <Col>
+
+                      <div class="col-xs-1" align="center">
+
+                        <Button class="cardButton">Go to Screenings for {savedFilm.film_title} </Button>
+                      </div>
+                    </Col>
+                  </Row>
+
+                </Container>
+              </Card.Body>
+
+            </Card>
+          </Col>
+    </Row>
+
+      )
+    }
+
+    else
+    {
+      shownCardsAdder(printArray, filmArray);
+    }
+  })
+
+  function shownCardsAdder(printArray, filmArray) {
+    printArray.push(
+      <Row>
+        {filmArray.map((i) => {
+          return (
+            <Col className=" d-flex align-items-center justify-content-center">
+  
+              <Link to={`/screenings/${i.film_title.replace(/\s+/g, '')}`}>
+              <Card style={{ width: "36rem" }} className="hp-card">
+                <Card.Body>
+                  <Container>
+                    <Row>
+                      <Col>
+                        <Card.Img variant="top" src={`${i.film_poster}`} />
+  
+                      </Col>
+  
+                      <Col>
+  
+                        <div class="col-xs-1" align="center">
+  
+                          <Button class="cardButton">Go to Screenings</Button>
+                        </div>
+                      </Col>
+                    </Row>
+  
+                  </Container>
+                </Card.Body>
+  
+              </Card>
+              </Link>
+            </Col>
+          );
+        })}
+      </Row>
+    );
+  }
+
+
+  if (loaded) {
+    console.log("68890");
+    if (error !== "") {
+      console.log(error);
+    } else if (showingData !== "") {
+      console.log(printArray);
+      return (
+        <>
+          {printArray}
+        </>
+      );
+
+    }
+  } else {
+    console.log("68891");
+  }
 };
 
 export default FilledScreening;
+
+
+
