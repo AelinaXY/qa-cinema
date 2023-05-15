cb56b322f77f3683a1e0d94f3cb9bf6211e72571
marsho23 <Mshouket@qa.com>
2023-05-15 12:39:42 +0100

    add database cleanup after test

--- Back-End/Tests/filmsTests.js
+++ Back-End/Tests/filmsTests.js
@@ -3,6 +3,9 @@ const chaiHttp = require('chai-http');
 const server = require('../index.js'); 
 const Films = require('../models/films'); 
 const app = require('../index.js');
+const dbConnect = require('../dbutils/dbConnect.js');
+const { DB } = require('../dbutils/dbconfig.js');
+const dbconfig = require('../dbutils/dbconfig.js');
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -25,10 +28,11 @@ describe('Film API', () => {
       .end((err, res) => {
         chai.expect(err).to.be.null;                
         chai.expect(res.body).to.include(filmData); 
-        chai.expect(res.status).to.equal(201);
+        chai.expect(res.status).to.equal(200);
 
         cleanUpDb()
         .then(()=>{
+          dbConnect.query("DELETE * FROM films where film_title='TestFilm'");
           done();
         })
         .catch((cleanupError)=>{
