b8923af4c2b030b60084c311dbe597f26d437daa
marsho23 <126670877+marsho23@users.noreply.github.com>
2023-05-16 08:41:36 +0100

    Update filmsTests.js to include getById, create, getAll and update tests

--- Back-End/Tests/filmsTests.js
+++ Back-End/Tests/filmsTests.js
@@ -38,4 +38,94 @@ describe('Film API', () => {
         done();
       });
   });
+
+  const expectedFilmAtId2 = {
+    id:2,
+    film_title: 'Joker',              
+    film_year: 2019,                        
+    film_rating: '15',                      
+    film_genre: 'Crime',                     
+    film_secondary_genre: 'Drama',         
+    film_poster: 'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg'        
+  };
+
+  it('should retrieve all films', (done) => {
+    chai
+      .request(app)
+      .get('/films/')
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        chai.expect(res.status).to.equal(200);
+        const responseBody = JSON.stringify(res.body); 
+        const expectedFilm = JSON.stringify(expectedFilmAtId2); 
+        chai.expect(responseBody).to.include(expectedFilm); 
+        done();
+      });
+  })
+
+  it('should retrieve a film by ID', (done) => {
+    const filmId = '2'; 
+  
+    chai
+      .request(app)
+      .get(`/films/byId/${filmId}`)
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        const responseBody = JSON.stringify(res.body); 
+        const expectedFilm = JSON.stringify(expectedFilmAtId2); 
+        chai.expect(responseBody).to.include(expectedFilm); 
+        chai.expect(res.status).to.equal(200);
+        done();
+      });
+  });
+  
+  it('should update a film by ID', (done) => {
+    const filmId = '2'; 
+    const updatedFilmData = {
+      id:2,
+      film_title: 'UpdatedFilm',              
+      film_year: 2023,                        
+      film_rating: '12A',   
+      film_genre: 'genre',                     
+      film_secondary_genre: 'secondary_genre',         
+      film_poster: 'UpdatedFilm film_poster'                     
+    };
+  
+    let originalFilmData; 
+    // retrieving the original film data
+    chai
+      .request(app)
+      .get(`/films/byId/${filmId}`)
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        chai.expect(res.status).to.equal(200);
+        originalFilmData = res.body; 
+      });
+  
+    // update test
+    chai
+      .request(app)
+      .put(`/films/${filmId}`)
+      .send(updatedFilmData)
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        const responseBody = JSON.stringify(res.body); 
+        console.log(responseBody);
+        const updatedFilm = JSON.stringify(updatedFilmData); 
+        chai.expect(responseBody).to.include(updatedFilm); 
+        chai.expect(res.status).to.equal(200);
+        done();
+      });
+  
+    // restoring the original film data
+    after(() => {
+      chai
+        .request(app)
+        .put(`/films/${filmId}`)
+        .send(originalFilmData) 
+        .end((err, res) => {
+          chai.expect(err).to.be.null;
+        });
+    });
+  });
 });
