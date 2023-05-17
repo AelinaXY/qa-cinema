375b870f33bf19474032d2205617d19721a22216
jimbobding <jimmygrahamdavid@gmail.com>
2023-05-17 22:06:53 +0100

    added info to places cards

--- Front-End/src/components/PlacesToEat.jsx
+++ Front-End/src/components/PlacesToEat.jsx
@@ -5,10 +5,11 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const PlacesToEat = () => {
   return (
-    <Container>
+    <Container className="places-Container">
       <Row>
         <Col>
           {" "}
+          <h1 className="text-center">Places to eat and drink </h1>
           <CardGroup>
             <Card className="places-card">
               <Card.Img
@@ -18,7 +19,12 @@ const PlacesToEat = () => {
               />
 
               <Card.Body>
-                <Card.Title>10 Tib Lane</Card.Title>
+                <Card.Title>
+                  {" "}
+                  <a target="_blank" href="https://www.10tiblane.com/">
+                    10 Tib Lane
+                  </a>{" "}
+                </Card.Title>
                 <Card.Text>
                   Classic cocktails, natural wine that doesn’t scare you off,
                   beer that matters and seasonal food with the best produce we
@@ -26,7 +32,9 @@ const PlacesToEat = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">
+                  10 Tib Ln, Manchester M2 4JB
+                </small>
               </Card.Footer>
             </Card>
             <Card className="places-card">
@@ -35,15 +43,27 @@ const PlacesToEat = () => {
                 variant="top"
                 src="/images/rudies.jpeg"
               />
+
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>
+                  <a
+                    target="_blank"
+                    href="https://www.rudyspizza.co.uk/pizzerias/peterst"
+                  >
+                    {" "}
+                    Rudys
+                  </a>
+                </Card.Title>
                 <Card.Text>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.{" "}
+                  We're a relaxed neighbourhood pizzeria, following the
+                  traditions and artistry of pizza from Naples - the birth place
+                  of pizza.
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">
+                  Petersfield House, Peter St, Manchester M2 5QJ
+                </small>
               </Card.Footer>
             </Card>
             <Card className="places-card">
@@ -53,15 +73,26 @@ const PlacesToEat = () => {
                 src="/images/schloss.jpeg"
               />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>
+                  {" "}
+                  <a
+                    target="_blank"
+                    href="https://www.albertsschloss.co.uk/manchester/"
+                  >
+                    Albert Schloss
+                  </a>{" "}
+                </Card.Title>
                 <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
+                  Bavarian-style beer cellar for traditional German food and
+                  pilsner beers on tap and in bottles. German food, beer and
+                  lots of live music.
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">
+                  {" "}
+                  27 Peter St, Manchester M2 5QR
+                </small>
               </Card.Footer>
             </Card>
           </CardGroup>
@@ -73,14 +104,22 @@ const PlacesToEat = () => {
                 src="/images/blinker.jpeg"
               />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>
+                  {" "}
+                  <a target="_blank" href="https://www.blinkerbar.co.uk/">
+                    {" "}
+                    Blinker
+                  </a>
+                </Card.Title>
                 <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  Blink
+                  Seasonal fruits used to create a menu that changes every
+                  month. Exceptional cocktails, brilliant service and fun times.
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">
+                  64-72 Spring Gardens, Manchester M2 2BQ
+                </small>
               </Card.Footer>
             </Card>
             <Card className="places-card">
@@ -90,14 +129,27 @@ const PlacesToEat = () => {
                 src="/images/mulligans.jpeg"
               />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>
+                  {" "}
+                  <a
+                    target="_blank"
+                    href="http://www.mulligansofdeansgate.com/"
+                  >
+                    Mulligans
+                  </a>{" "}
+                </Card.Title>
                 <Card.Text>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.{" "}
+                  Mulligans, based just off Deansgate in the heart of
+                  Manchester, is an Authentic Irish Pub, renowned for having the
+                  best pint of Guinness in Manchester and is Manchester’s oldest
+                  Irish Pub.
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">
+                  {" "}
+                  12 Southgate, Manchester M3 2RB
+                </small>
               </Card.Footer>
             </Card>
             <Card className="places-card">
@@ -107,15 +159,23 @@ const PlacesToEat = () => {
                 src="/images/schofields.webp"
               />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>
+                  {" "}
+                  <a target="_blank" href="https://www.schofieldsbar.com/">
+                    Schofields
+                  </a>{" "}
+                </Card.Title>
                 <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
+                  With over 25 years of experience collectively, bartender
+                  brothers Joe and Daniel Schofield always had a vision to
+                  return to their home city of Manchester to open their
+                  eponymous venue; SCHOFIELD’S BAR.
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">
+                  3 Little Quay Street Sunlight House, Manchester M3 3JZ
+                </small>
               </Card.Footer>
             </Card>
           </CardGroup>
